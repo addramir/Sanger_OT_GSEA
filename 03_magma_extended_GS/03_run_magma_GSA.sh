b0ac0ad51f59adbca0633e4364dc6e5b5f67cbd6
Yakov Tsepilov <yt4@sanger.ac.uk>
2023-01-25 09:24:05 +0000

    GS extended GSA

--- 03_magma_extended_GS/03_run_magma_GSA.sh
+++ 03_magma_extended_GS/03_run_magma_GSA.sh
@@ -1,15 +1,26 @@
-cd ~/projects/GSEA/03_magma_124IDs
+cd /mnt/disk1/data/GS_233IDs
 
+path2csv=/mnt/disk1/data/GS_233IDs/to_magma/
+path2save=/mnt/disk1/data/GS_233IDs/GSA_out/
+path2gba=/mnt/disk1/data/GS_233IDs/GSA_magma/
 
-path2csv=/mnt/disk1/data/GS_124IDs/to_magma/
-path2save=/mnt/disk1/data/GS_124IDs/GSA_out/
-gw=$(ls $path2csv)
+cust_func(){
 
-for g in $gw
-do
-echo $g
+path2csv=/mnt/disk1/data/GS_233IDs/to_magma/
+path2save=/mnt/disk1/data/GS_233IDs/GSA_out/
+path2gba=/mnt/disk1/data/GS_233IDs/GSA_magma/
+
 ~/projects/GSEA/02_magma/magma \
---gene-results /mnt/disk1/data/GS_124IDs/GSA_magma/$g.genes.raw \
+--gene-results $path2gba/"$1".genes.raw \
 --set-annot ~/projects/GSEA/03_magma_124IDs/10667_GSs_for_magma_ENSG.txt \
---out $path2save/$g
-done
+--out $path2save/"$1"
+
+}
+
+cd /mnt/disk1/data/GS_233IDs/
+
+export -f cust_func
+
+ls $path2csv > csv_list.txt
+
+cat csv_list.txt | parallel -j 16 cust_func {}
